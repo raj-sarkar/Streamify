79e9776cf266af31bd73c7685f8a089ec7f1e0a8
Raj Sarkar <raj1451a1451@gmail.com>
2026-04-30 19:12:29 +0530

    modified response of currentUser controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -54,7 +54,7 @@ export const logout = (req, res) => {
 
 export const current = async (req, res) => {
     try {
-        res.status(200).json({user:req.user});
+        res.status(200).json(req.user);
     } catch (error) {
         res.status(500).json({
             message: error.message || "Internal server error"
